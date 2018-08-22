e79a9bb598a1d1a6c775599a50af704138cea324
Dave Willenberg <dave@detroit-english.de>
2018-08-22 03:31:26 +0200

    Add script, verbose, clearRoute config options; better input validation

--- src/index.js
+++ src/index.js
@@ -1,58 +1,117 @@
 import 'colors'
 import cfMethods from './lib'
+import fs from 'fs'
+import path from 'path'
 
 class CloudflareWorkerPlugin {
   constructor(
     authEmail = null,
     authKey = null,
-    { zone = null, enabled = true, pattern }
+    { zone = null, enabled = true, script, pattern, clearRoutes, verbose }
   ) {
-    const requiredParams = {
+    const requiredConfig = {
       'CF-Account-Email': authEmail,
       'CF-API-Key': authKey,
       zone,
     }
-    for (let [key, value] of Object.entries(requiredParams)) {
+    for (let [key, value] of Object.entries(requiredConfig)) {
+      if (!value) {
+        throw new Error(`'${key}' is undefined`)
+      }
       if (typeof value !== 'string') {
-        throw new Error(`'${key}' either missing, or not a string`.red)
+        throw new Error(`'${key}' is not a string`)
       }
     }
-    if ({ pattern }.hasOwnProperty('pattern') && typeof pattern !== 'string') {
-      throw new Error(`'pattern' must be a string.`.red)
+    for (let [key, value] of Object.entries({ script, pattern })) {
+      if (value && typeof value !== 'string') {
+        throw new Error(`'${key}' must be a string`)
+      }
     }
-    this._enabled = !!enabled
+    this._script =
+      script && enabled ? path.normalize(`${process.cwd()}/${script}`) : void 0
     this._pattern = pattern
+    this._enabled = enabled === true
     this._cfMethods = enabled ? { ...cfMethods(authEmail, authKey, zone) } : {}
+    this._clearRoutes = clearRoutes === true
+    this._verbose = verbose === true
   }
 
-  async disableExistingRoutes() {
-    let { result } = await this._cfMethods.getRoutes()
-    const matchingResult = result.find(r => r.pattern === this._pattern)
-    if (matchingResult) {
-      await this._cfMethods.deleteRoute(matchingResult)
-      result = result.filter(r => r.pattern !== this._pattern)
+  logg(stuff, color = `cyan`) {
+    if (!this._verbose) return void 0
+    return typeof stuff === 'object'
+      ? console.info(`${JSON.stringify(stuff, null, 2)}`[color])
+      : console.info(`${stuff}`[color])
+  }
+
+  async processExistingRoutes() {
+    let matchingRoute
+    let shouldCreateNewRoute = true
+    let { result: existingRoutes } = await this._cfMethods.getRoutes()
+
+    if (this._clearRoutes) {
+      this.logg(
+        `Deleting all routes: ${existingRoutes.map(r => r.pattern).join(', ')}`
+      )
+      const deletedRoutes = await Promise.all(
+        existingRoutes.map(this._cfMethods.deleteRoute)
+      )
+      this.logg(`Deleted patterns: ${deletedRoutes.join(', ')}`.red)
+      return true
     }
-    await Promise.all(result.map(this._cfMethods.disableRoute))
+
+    const matchIndex = existingRoutes.findIndex(
+      r => r.pattern === this._pattern
+    )
+
+    if (matchIndex > -1) {
+      shouldCreateNewRoute = false
+      matchingRoute = existingRoutes.splice(matchIndex, 1).pop()
+      if (matchingRoute.enabled) {
+        this.logg(`Pattern already enabled: ${matchingRoute.pattern}`, `green`)
+      } else {
+        this.logg(
+          `Re-enabling exiting pattern: ${matchingRoute.pattern}`,
+          `green`
+        )
+        const enabledRoute = await this._cfMethods.enableRoute(matchingRoute)
+        console.info(`Enabled route pattern: ${enabledRoute}`.green)
+      }
+    }
+
+    const disabledRoutes = await Promise.all(
+      existingRoutes.map(this._cfMethods.disableRoute)
+    ).then(arr => arr.filter(Boolean))
+
+    if (disabledRoutes.length) {
+      this.logg(`Disabled patterns: ${disabledRoutes.join(', ')}`.yellow)
+    }
+
+    return shouldCreateNewRoute
   }
 
   async upsertNewPattern() {
-    await this.disableExistingRoutes()
+    const shouldCreateNewRoute = await this.processExistingRoutes()
+    if (!shouldCreateNewRoute) return
     await this._cfMethods.createRoute(this._pattern)
   }
 
   apply(compiler) {
-    return compiler.hooks.emit.tapPromise(
+    return compiler.hooks.afterEmit.tapPromise(
       'CloudflareWorkerPlugin',
       async compilation => {
         if (!this._enabled)
           return console.info(`Cloudflare deployment disabled.`.yellow)
+        let filename, code
         try {
-          const { filename } = compilation.outputOptions
-          const workerScript = compilation.assets[filename].source()
+          filename = this._script || compilation.outputOptions.filename
+          code = compilation.assets[filename]
+            ? compilation.assets[filename].source()
+            : fs.readFileSync(filename).toString()
           if (this._pattern) {
             await this.upsertNewPattern()
           }
-          return this._cfMethods.uploadWorker(Buffer.from(workerScript))
+          this.logg(`Uploading worker...`, `green`)
+          return this._cfMethods.uploadWorker(Buffer.from(code))
         } catch (err) {
           console.error(`${err.message}`.red)
           throw err
