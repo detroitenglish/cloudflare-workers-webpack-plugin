5919154c06a92ab1a0eb1c8d9badc66938f78016
Dave Willenberg <dave@detroit-english.de>
2018-08-22 03:27:51 +0200

    Improve error handling/reporting

--- src/lib/bootstrap.js
+++ src/lib/bootstrap.js
@@ -10,16 +10,22 @@ export default function(cfMail, cfKey, zoneId) {
       'X-Auth-Email': cfMail,
       'X-Auth-Key': cfKey,
     },
+    timeout: 2e4,
   })
   instance.interceptors.response.use(
     response => response.data,
     err => {
       const errors = err?.response?.data.errors
 
-      if (errors && Array.isArray(errors))
-        errors.forEach(error =>
-          console.error(`[code ${error.code}]: ${error.message}`.red)
-        )
+      if (errors && Array.isArray(errors)) {
+        errors.forEach(error => {
+          if (error.message)
+            console.error(`[code ${error.code}]: ${error.message}`.red)
+          else console.error(`${JSON.stringify(error, null, 2)}`.red)
+        })
+      } else {
+        console.error(err)
+      }
       throw err
     }
   )
