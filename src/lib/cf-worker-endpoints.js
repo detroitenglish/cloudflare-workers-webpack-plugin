11582a2eae376af175b02b34cbe17caabec60083
Dave Willenberg <dave@detroit-english.de>
2018-08-27 00:02:35 +0200

    Remove in-function logging

--- src/lib/cf-worker-endpoints.js
+++ src/lib/cf-worker-endpoints.js
@@ -5,8 +5,7 @@ export default function(ax) {
   async function uploadWorker(data) {
     const scriptSize = Math.floor(data.byteLength / 1024)
     if (scriptSize > 1000) {
-      console.error(`Script size is ${scriptSize}KB`.red)
-      throw new Error(`CF-Worker script size limit exceeded`)
+      throw new Error(`CF-Worker script size limit exceeded (${scriptSize}KB)`)
     }
     await ax({
       url: `/workers/script`,
@@ -14,7 +13,5 @@ export default function(ax) {
       headers: { 'content-type': 'application/javascript' },
       data,
     })
-
-    return console.log(`Success! Worker script uploaded 🚀`.green)
   }
 }
